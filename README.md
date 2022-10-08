d7519de62e61b72dbc63f9c627a40b60aad234ec
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-10-08 08:53:53 +0200

    Add first version of Address class

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # address-book
 Application for lab 2 in 1DV610, LNU 2022.
+
+## Development notes
+
+- Re-configured lint to ignore missing JSDoc-comments
---
+++ src/model/domain/Address.js
@@ -0,0 +1,88 @@
+/**
+ * Represents a physical address.
+ */
+export class Address {
+  #streetName
+  #houseNumber
+  #postalCode
+  #city
+  #country
+  #addressType
+
+  #ADDRESS_TYPE_ENUMS = [
+    'Home',
+    'Work',
+    'Temporary'
+  ]
+
+  setStreetName (streetName) {
+    this.#validateString(streetName)
+    this.#streetName = streetName
+  }
+
+  getStreetName () {
+    return this.#streetName
+  }
+
+  setHouseNumber (houseNumber) {
+    this.#validateString(houseNumber)
+    this.#houseNumber = houseNumber
+  }
+
+  getHouseNumber () {
+    return this.#houseNumber
+  }
+
+  setPostalCode (postalCode) {
+    this.#validateString(postalCode)
+    this.#postalCode = postalCode
+  }
+
+  getPostalCode (postalCode) {
+    return this.#postalCode
+  }
+
+  setCity (city) {
+    this.#validateString(city)
+    this.#city = city
+  }
+
+  getCity () {
+    return this.#city
+  }
+
+  setCountry (country) {
+    this.#validateString(country)
+    this.#country = country
+  }
+
+  getCountry () {
+    return this.#country
+  }
+
+  #validateString (validationString) {
+    const error = new TypeError('Must be a string with at least 1 character.')
+
+    if (typeof validationString !== 'string') throw error
+    if (validationString.length < 1) throw error
+  }
+
+  setAddressType (addressType) {
+    this.#validateAddressType(addressType)
+    this.#addressType = addressType
+  }
+
+  getAddressType () {
+    return this.#addressType
+  }
+
+  static getAddressTypeEnums () {
+    return Array.from(this.#ADDRESS_TYPE_ENUMS)
+  }
+
+  #validateAddressType (addressType) {
+    if (!this.#ADDRESS_TYPE_ENUMS.includes(addressType)) {
+      throw new TypeError('addressType must be one of Address.getAddressTypeEnums().')
+    }
+  }
+}
